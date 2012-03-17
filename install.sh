aec7fd13df86652cc24cc21addc8e0c16c93e519
Sébastien Béal <sebastibe@gmail.com>
2012-03-17 09:26:44 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
